8ded7915f68541a337916213dfd71ffa3188b7fd
Bernd <bernd@elbstacks-MBP-2.fritz.box>
2020-09-14 11:07:36 +0200

    inital commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Oracle and Transaction Service
+
+## Purpose
+The oracle part is for getting information from the bitcoin blockchain. The Transaction Service is to create transaction in the BTC network.
+
+## Usage
+You need a bcoin API. 
+Environment: 
+- BCOIN_URL: url with http prefix of the api
+- BCOIN_PASSWORD: bcoin api token
+- REQUIRED_CONFIRMATIONS: (default 6) confirmations required to deem a block as accepted.
+
+`yarn start`
+
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import { FullServiceHandlers, AssetTransactionData } from './types';
+import { BcoinHandlers, BlockcyperHandlers } from './implementations';
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+let handlers: FullServiceHandlers;
+switch (process.env.SOURCE) {
+  case 'BCOIN': {
+    console.log('USING BCOIN')
+    handlers = BcoinHandlers;
+    break;
+  }
+  case 'BLOCKCYPHER': {
+    console.log('USING BLOCKCYPHER')
+    handlers = BlockcyperHandlers;
+    break;
+  }
+  default:
+    console.log('USING BCOIN')
+    handlers = BcoinHandlers;
+}
+
+app.get('/oracle/transactionInfo', async (req, res) => {
+  const { reference, poolAddress } = req.query;
+  if (!reference || !poolAddress) return res.json({ status: 'ERROR' });
+  const result = await handlers.oracle.getTransactionInformation(reference as string, poolAddress as string);
+  return res.json(result);
+});
+
+app.post('/oracle/validateSignature', async (req, res) => {
+  const { message, address, signature } = req.body;
+  if (!message || !address || !signature) {
+    return res.status(400).send('Missing body');
+  }
+  const result = await handlers.oracle.validateSignature(message, address, signature);
+  return res.json(result);
+});
+
+app.post('/tx/create', async (req, res) => {
+  const { transactionData } = req.body;
+  if (!transactionData) {
+    return res.status(400).send('Missing parameters');
+  }
+  const result = await handlers.transactionService.createTransaction(transactionData as AssetTransactionData[]);
+  return res.json(result);
+});
+
+app.post('/tx/signAndSend', async (req, res) => {
+  const { partialTx, signatures } = req.body;
+  if (!partialTx || !signatures) {
+    return res.status(400).send('Missing body');
+  }
+  const result = await handlers.transactionService.signAndSendTransaction(partialTx, signatures);
+  return res.json(result);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port);
+console.log(`Service listening on port ${port}`);
