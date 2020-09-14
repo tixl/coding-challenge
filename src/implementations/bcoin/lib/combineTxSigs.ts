1be47488b19e1f7e15b2f75114cbdb0784d8b1d2
Bernd <bernd@elbstack.com>
2020-09-14 11:33:30 +0200

    update description

--- src/implementations/bcoin/lib/combineTxSigs.ts
+++ src/implementations/bcoin/lib/combineTxSigs.ts
@@ -1,8 +1,10 @@
 import { PartialTransaction, FullTransaction } from '../../../types';
 
-export async function combineTxSigs(_partialTx: PartialTransaction, _signatures: string[], _signerPublicKey: string): Promise<FullTransaction> {
+export async function combineTxSigs(_partialTx: PartialTransaction, _signatures: string[], _publicKeys: string[], _signerPublicKey: string): Promise<FullTransaction> {
   // TODO: Implement
   // Takes the signatures of the tosigns created by the 'getTosigns" method and the transaction template
   // to create a complete transaction that is ready to be sent to the network
+  // signatures will be delivered as DER in hex
+  // public keys can be delivered in 33 byte compressed, 65-byte hybrid or 65-byte uncompressed format (choose one)
   return {};
 }
